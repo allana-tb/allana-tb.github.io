bfebca126cc7ec4a5233b4aefa6edf3775fa0270
Allana <allanatavaresb@gmail.com>
2022-02-01 23:57:31 -0300

    .

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
 
-///////HOME
-// Scrolling effect, transitioning from full screen and resizing
-window.onscroll = function() {scrollfunction()};
-
- function scrollfunction() {
-     if (document.body.scrollTop >= 250 ||
-        document.socumentElement.scrollTop >= 250) {
-            document.getElementById("home").style.fontSize = "90px";
-        } else {
-            document.getElementById("home").style.fontSize = "45px";
-            document.getElementById("home").style.display = "flex";
-            document.getElementById("home").style.flex = "50%";
-            removeClass('home__subtitle');
-        }
- }
-
  function onReady(callback) {
     var intervalID = window.setInterval(checkReady, 1000);
 
