4e93fd19d3d2337a9d2b6b1cc93e88752a7f0a4b
Allana <allanatavaresb@gmail.com>
2023-01-16 20:15:36 -0300

    Dark mode

---
+++ App.js
@@ -0,0 +1,35 @@
+var toggleSwitch = document.querySelector(".checkbox");
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("color-scheme", "dark");
+    /*If required*/
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+  } else {
+    document.documentElement.setAttribute("color-scheme", "light");
+    /*If required*/
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+            
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
