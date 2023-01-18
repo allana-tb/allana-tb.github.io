c64ac479dfe13b45c0886057937c18d2b445476b
Allana <allanatavaresb@gmail.com>
2023-01-18 01:28:09 -0300

    update header

--- App.js
+++ App.js
@@ -2,19 +2,30 @@ var toggleSwitch = document.querySelector(".checkbox");
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("color-scheme", "dark");
-    /*If required*/
-    document.body.classList.add("dark-mode");
-    document.body.classList.remove("light-mode");
-  } else {
     document.documentElement.setAttribute("color-scheme", "light");
     /*If required*/
     document.body.classList.add("light-mode");
     document.body.classList.remove("dark-mode");
+  } else {
+    document.documentElement.setAttribute("color-scheme", "dark");
+    /*If required*/
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
   }
 }
 
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+
+const checkbox = document.getElementById("checkbox");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: light)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
 
 
 
